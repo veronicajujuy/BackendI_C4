4a39b360c155d5272e18c4ebca83e1888899d60b
veronicajujuy <veronica.jujuy@gmail.com>
2024-05-17 00:04:57 -0300

    finalizando clase 11

--- Clase_11/claserepaso/src/dh/backend/test/PacienteEnMemoriaTest.java
+++ Clase_11/claserepaso/src/dh/backend/test/PacienteEnMemoriaTest.java
@@ -1,56 +1,36 @@
 package dh.backend.test;
-
 import dh.backend.dao.impl.PacienteDaoH2;
+import dh.backend.dao.impl.PacienteEnMemoria;
 import dh.backend.model.Domicilio;
 import dh.backend.model.Paciente;
 import dh.backend.service.PacienteService;
-import org.apache.log4j.Logger;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-class PacienteServiceTest {
-    private static Logger LOGGER = Logger.getLogger(PacienteServiceTest.class);
-    private static PacienteService pacienteService = new PacienteService(new PacienteDaoH2());
-    @BeforeAll
-    static void crearTablas(){
-        Connection connection = null;
-        try {
-            Class.forName("org.h2.Driver");
-            connection = DriverManager.getConnection("jdbc:h2:~/db_clinica_1605;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOGGER.error(e.getMessage());
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                LOGGER.error(e.getMessage());
-            }
-        }
-    }
+public class PacienteEnMemoriaTest {
+    private static PacienteService pacienteService = new PacienteService(new PacienteEnMemoria());
 
     @Test
     @DisplayName("Testear que un paciente fue guardado")
     void testPacienteGuardado(){
         Paciente paciente = new Paciente("Cosme","Menganito","456464", LocalDate.of(2024,04,22),
                 new Domicilio("Calle Falsa", 456, "Springfield","Montana"));
-        Paciente pacienteDesdeLaBD = pacienteService.registrarPaciente(paciente);
+        Paciente pacienteDesdeLaMemoria = pacienteService.registrarPaciente(paciente);
 
-        assertNotNull(pacienteDesdeLaBD);
+        assertNotNull(pacienteDesdeLaMemoria);
     }
 
     @Test
     @DisplayName("Testear busqueda paciente por id")
     void testPacientePorId(){
+        Paciente paciente = new Paciente("Cosme","Menganito","456464", LocalDate.of(2024,04,22),
+                new Domicilio("Calle Falsa", 456, "Springfield","Montana"));
+        pacienteService.registrarPaciente(paciente);
+
         Integer id = 1;
         Paciente pacienteEncontrado = pacienteService.buscarPorId(id);
 
@@ -66,9 +46,7 @@ class PacienteServiceTest {
 
         List<Paciente> pacientes = pacienteService.buscarTodos();
 
-        assertEquals(2, pacientes.size());
+        assertEquals(1, pacientes.size());
 
     }
-
-
 }
