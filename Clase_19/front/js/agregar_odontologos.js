486aeb8547b5d15d5b38fa5ed50a86c3183ae8de
veronicajujuy <veronica.jujuy@gmail.com>
2024-06-04 22:45:06 -0300

    agregando clase 19

---
+++ Clase_19/front/js/agregar_odontologos.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("agregarForm");
+const apiURL = "http://localhost:8080";
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const matricula = document.getElementById("matricula").value;
+
+  // llamando al endpoint de agregar
+
+  fetch(`${apiURL}/odontologo`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, apellido, nroMatricula: matricula }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      alert("Odontólogo agregado con éxito");
+      form.reset(); // Resetear el formulario
+    })
+    .catch((error) => {
+      console.error("Error agregando odontólogo:", error);
+    });
+});
