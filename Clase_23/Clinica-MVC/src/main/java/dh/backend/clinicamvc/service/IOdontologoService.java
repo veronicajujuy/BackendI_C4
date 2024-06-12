84fb9a0bcf5d2cb2721855cede08ab689f8eb926
veronicajujuy <veronica.jujuy@gmail.com>
2024-06-11 22:41:01 -0300

    agregando clase 23

---
+++ Clase_23/Clinica-MVC/src/main/java/dh/backend/clinicamvc/service/IOdontologoService.java
@@ -0,0 +1,21 @@
+package dh.backend.clinicamvc.service;
+
+
+import dh.backend.clinicamvc.entity.Odontologo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IOdontologoService {
+    Odontologo agregarOdontologo(Odontologo odontologo);
+
+    Optional<Odontologo> buscarUnOdontologo(Integer id);
+    List<Odontologo> buscarTodosOdontologos();
+
+    void modificarOdontologo(Odontologo odontologo);
+    void eliminarOdontologo(Integer id);
+
+    // Metodos con HQL
+    List<Odontologo> buscarPorApellido(String apellido);
+    List<Odontologo> buscarPorNombre(String nombre);
+}
---
+++ Clase_23/Clinica-MVC/src/main/java/dh/backend/clinicamvc/service/impl/OdontologoService.java
@@ -0,0 +1,51 @@
+package dh.backend.clinicamvc.service.impl;
+
+
+import dh.backend.clinicamvc.entity.Odontologo;
+import dh.backend.clinicamvc.repository.IOdontologoRepository;
+import dh.backend.clinicamvc.service.IOdontologoService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OdontologoService implements IOdontologoService {
+
+    private IOdontologoRepository odontologoRepository;
+
+    public OdontologoService(IOdontologoRepository odontologoRepository) {
+        this.odontologoRepository = odontologoRepository;
+    }
+
+    public Odontologo agregarOdontologo(Odontologo odontologo){
+        return odontologoRepository.save(odontologo);
+    }
+
+    public Optional<Odontologo> buscarUnOdontologo(Integer id){
+        return odontologoRepository.findById(id);
+    }
+    public List<Odontologo> buscarTodosOdontologos(){
+        return odontologoRepository.findAll();
+    }
+
+    @Override
+    public void modificarOdontologo(Odontologo odontologo) {
+        odontologoRepository.save(odontologo);
+    }
+
+    @Override
+    public void eliminarOdontologo(Integer id) {
+        odontologoRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Odontologo> buscarPorApellido(String apellido) {
+        return odontologoRepository.buscarPorApellido(apellido);
+    }
+
+    @Override
+    public List<Odontologo> buscarPorNombre(String nombre) {
+        return odontologoRepository.findByNombreLike(nombre);
+    }
+}
